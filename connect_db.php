b1f6db6eec767a348b0cee056b92c894e4bf895b
WATTANA <wattana.po@ku.th>
2023-02-04 16:24:09 +0700

    sing

--- connect_db.php
+++ connect_db.php
@@ -2,13 +2,12 @@
 $servername = "localhost";
 $username = "cpe3377";
 $password = "123456";
+$dbname = "cpe3377";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+};
 ?>
--- list.php
+++ list.php
@@ -1,15 +1,6 @@
 <?php
-$servername = "localhost";
-$username = "cpe3377";
-$password = "123456";
-$dbname = "cpe3377";
+require("connect_db.php");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
 $sql = "SELECT id, name, age, gender, marry_status FROM WATTANA";
 $result = $conn->query($sql);
