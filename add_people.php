0b657587ff3aef816c5e6ee57a028e4d026a1a4b
WATTANA <wattana.po@ku.th>
2023-02-18 11:29:43 +0700

    up

---
+++ add_people.php
@@ -0,0 +1,13 @@
+<form action="save_add_people.php" method="post">
+    Name: <input type="text" name="name"><br>
+    Age: <input type="text" name="age"><br>
+    Gender: <select name="gender">
+        <option value="m">Male</option>
+        <option value="f">Female</option>
+    </select><br>
+    Marry Status: <select name="marry_status">
+        <option value="s">Single</option>
+        <option value="m">Married</option>
+    </select><br>
+    <input type="submit" value="Send">
+</form>
